aaa72593a3fcd456415b1539c230be562059e748
AJ ONeal <awesome@coolaj86.com>
2015-07-09 15:11:10 -0600

    Update test-port.go

--- test-port.go
+++ test-port.go
@@ -25,23 +25,23 @@ func main() {
   port := args[0]
   _, err := strconv.ParseUint(port, 10, 16)
   if err != nil {
-    fmt.Fprintf(os.Stderr, "Invalid port %q: %s", port, err)
+    fmt.Fprintf(os.Stderr, "Invalid port %q: %s\n", port, err)
     os.Exit(1)
   }
 
   ln, err := net.Listen("tcp", ":" + port)
 
   if err != nil {
-    fmt.Fprintf(os.Stderr, "Can't listen on port %q: %s", port, err)
+    fmt.Fprintf(os.Stderr, "Can't listen on port %q: %s\n", port, err)
     os.Exit(1)
   }
 
   err = ln.Close()
   if err != nil {
-    fmt.Fprintf(os.Stderr, "Couldn't stop listening on port %q: %s", port, err)
+    fmt.Fprintf(os.Stderr, "Couldn't stop listening on port %q: %s\n", port, err)
     os.Exit(1)
   }
 
-  fmt.Printf("TCP Port %q is available", port)
+  fmt.Printf("TCP Port %q is available\n", port)
   os.Exit(0)
 }
